20e39d713197a8cc5d2bfa832a3f1fcee2e555e3
umair <cyumair@hotmail.com>
2021-11-06 20:02:43 +0500

    line height 1.5 in a few paras

--- src/Components/ProductsSection.js
+++ src/Components/ProductsSection.js
@@ -5,7 +5,7 @@ const ProductsSection = () => {
     return(
         <div className = 'pb-24 bg-dark' id = 'ourproduct'>
 
-            <section className = 'pt-24'>
+            <section className = 'pt-24 leading-normal'>
                 <Wrapper>
                     <div className = 'h-1 w-28 bg-custom-green '></div>
                     <h1 className = 'text-5xl mt-2 mb-4 font-bold'>Our Mission</h1>
@@ -29,7 +29,7 @@ const ProductsSection = () => {
                             </div>
                             <p className = 'ml-5 font-semibold'>BeatPlaylist</p>
                         </div>
-                        <div className = 'md:ml-12 max-w-500px text-graytext text-lg leading-normal'>
+                        <div className = 'md:ml-12 max-w-500px text-graytext text-lg'>
                             <p className = 'my-4'>BeatPlaylist is a desktop music streaming platform powered by YouTube that will be released in Beta during Q3, 2021.</p>
                             <p className = 'my-4'>FF9 will be integrated into BeatPlaylist as the platform's currency, powering our NFT albums, singles, playlists, and pins, and will offer wallet perks such as free premium to wallet holders of more than 10 million tokens and much more.</p>
                             <p className = 'my-4'>Learn more about FF9 and BeatPlaylist through our <a href = '/' className = 'hover:text-custom-green text-white'>Whitepaper.</a></p>
